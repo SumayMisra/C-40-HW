2e300189336db147fb6656b61c47abc8c6065b29
SumayMisra <63454749+SumayMisra@users.noreply.github.com>
2020-06-15 18:17:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+var drawingball;
+var database, pos, piece;
+
+var trajectory=[];
+
+function setup(){
+    createCanvas(500,500);
+
+    database=firebase.database();
+
+    peice=loadImage("piece.png");
+
+    drawingball = createSprite(250,250,10,10);
+    drawingball.shapeColor = "red";
+
+    database.ref("drawingball/position").on("value",readPos, showErr);
+}
+
+function draw(){
+    background("white");
+    drawSprites();
+
+    var position = [drawingball.x, drawingball.y];
+    trajectory.push(position);
+
+    for(var i=0; i<trajectory.length; i++){
+        image(piece, trajectory[i][0], trajectory[i][1]);
+    }
+
+    database.ref("drawingball").set({
+        drawings:trajectory
+    })
+}
+
+function changePosition(){
+    database.ref("drawingball/position").set({
+        x:mouseX,
+        y:mouseY
+    })
+}
+
+function mouseDragged(){
+    drawingball.x=mouseX;
+    drawingball.y=mouseY;
+
+    database.ref("drawingball/position").set({
+        x:mouseX,
+        y:mouseY
+    })
+}
+
+function mouseReleased(){
+}
+
+function readPos(data){
+    pos=data.val();
+    drawingball.x=pos.x;
+    drawingball.y=pos.y;
+}
+
+function showErr(){
+    console.log("Error OOCCUURREEDD");
+}
